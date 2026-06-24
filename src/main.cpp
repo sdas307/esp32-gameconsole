9ed823c469e02a0ccaf6fba2dca4f2a8666e7914
kingxavid <sdas307@users.noreply.github.com>
2026-06-24 23:48:06 +0530

    Indentation Fix

--- src/main.cpp
+++ src/main.cpp
@@ -2,35 +2,35 @@
 
 void setup()
 {
-  Serial.begin(9600);
-  Serial.println("Hello, ESP32!");
+    Serial.begin(9600);
+    Serial.println("Hello, ESP32!");
 
-  initHardware();
-  initDisplay();
-  showStartupScreen();
+    initHardware();
+    initDisplay();
+    showStartupScreen();
 }
 
 void loop()
 {
-  display.clearDisplay();
+    display.clearDisplay();
 
-  switch (currentState)
-  {
-  case STATE_HOME:
-    HomeManager();
-    break;
+    switch (currentState)
+    {
+    case STATE_HOME:
+        HomeManager();
+        break;
 
-  case STATE_GAME:
-    GameManager();
-    break;
+    case STATE_GAME:
+        GameManager();
+        break;
 
-  case STATE_SETTINGS:
-    SettingsManager();
-    break;
+    case STATE_SETTINGS:
+        SettingsManager();
+        break;
 
-  default:
-    break;
-  }
+    default:
+        break;
+    }
 
-  display.display();
+    display.display();
 }
